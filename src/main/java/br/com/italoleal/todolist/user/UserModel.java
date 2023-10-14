d0bfe81e3fc9eaa777cb0a5b90398936c3332289
Italo Leal <itlcoorporativo@gmail.com>
2023-10-14 19:31:10 -0300

    feat: Add lombok dependency to manager getters and setters

--- src/main/java/br/com/italoleal/todolist/user/UserModel.java
+++ src/main/java/br/com/italoleal/todolist/user/UserModel.java
@@ -1,32 +1,12 @@
 package br.com.italoleal.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
 
   private String username;
   private String name;
   private String password;
 
-  public void  setUsername(String username) {
-    this.username = username;
-  }
-
-    public String getUsername() {
-      return username;
-  }
-
-    public void  setName(String name) {
-    this.name = name;
-  }
-
-    public String getName() {
-      return name;
-  }
-
-    public void  setNPassword(String password) {
-    this.password = password;
-  }
-
-    public String getPassword() {
-      return password;
-  }
 }
